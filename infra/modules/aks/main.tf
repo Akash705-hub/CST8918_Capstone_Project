2014795dba6f74dd0b7213434575f99a734f1d47
harshdeep <harshdeep@mail.com>
2026-08-13 22:30:20 -0400

    fix: format terraform code

--- infra/modules/aks/main.tf
+++ infra/modules/aks/main.tf
@@ -7,15 +7,15 @@ resource "azurerm_kubernetes_cluster" "aks" {
   kubernetes_version = var.kubernetes_version
 
   default_node_pool {
-    name            = "default"
-    node_count      = var.node_count
+    name                 = "default"
+    node_count           = var.node_count
     auto_scaling_enabled = var.enable_auto_scaling
-    min_count       = var.enable_auto_scaling ? var.min_node_count : null
-    max_count       = var.enable_auto_scaling ? var.max_node_count : null
-    vm_size         = var.vm_size
-    vnet_subnet_id  = var.subnet_id
+    min_count            = var.enable_auto_scaling ? var.min_node_count : null
+    max_count            = var.enable_auto_scaling ? var.max_node_count : null
+    vm_size              = var.vm_size
+    vnet_subnet_id       = var.subnet_id
     upgrade_settings {
-         max_surge = "10%"
+      max_surge = "10%"
     }
   }
 
@@ -26,8 +26,8 @@ resource "azurerm_kubernetes_cluster" "aks" {
   network_profile {
     network_plugin    = "azure"
     load_balancer_sku = "standard"
-    service_cidr   = "10.10.0.0/16"
-    dns_service_ip = "10.10.0.10"
+    service_cidr      = "10.10.0.0/16"
+    dns_service_ip    = "10.10.0.10"
   }
 
   tags = var.tags
@@ -35,7 +35,7 @@ resource "azurerm_kubernetes_cluster" "aks" {
 
 # Role assignment for AKS cluster to pull images from ACR
 resource "azurerm_role_assignment" "aks_acr_pull" {
-  scope              = var.acr_id
+  scope                = var.acr_id
   role_definition_name = "AcrPull"
-  principal_id       = azurerm_kubernetes_cluster.aks.kubelet_identity[0].object_id
+  principal_id         = azurerm_kubernetes_cluster.aks.kubelet_identity[0].object_id
 }
